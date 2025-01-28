7c4adf8ebcb05e42e96a1550fe30531c3b152eae
rayane moara <rayane_moara@hotmail.com>
2025-01-28 11:51:12 -0300

    Alteração home e css

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,19 @@
+import styles from './Home.module.css';
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Seja Bem Vindo à GenFit!</h2>
+        <p className={styles.description}>Aprenda os melhores exercícios e tenha qualidade de vida.</p>
+        <img
+          src="https://down-br.img.susercontent.com/file/4042fd5c3a4353057c3407ee83e1eb14"
+          alt="Imagem da Página Home"
+          className={styles.image}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
