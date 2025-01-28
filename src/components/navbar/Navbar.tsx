3323e994fa6cdc110ebcf3da0194a23fddd35acd
Kevin Schuab <kevinschuab@gmail.com>
2025-01-28 16:23:42 -0300

    estilização

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <>
@@ -6,15 +8,33 @@ function Navbar() {
             			   bg-black text-orange-400 font-bold border-orange-400 border-b-2"
       >
         <div className="container flex justify-between text-lg">
+          <Link to="/home" className="hover:text-[#5D4037]">
+            <div className="flex gap-4 items-center hover:text-[#5D4037]">
+              <img
+                src="https://files.oaiusercontent.com/file-Hr8fa69sCxnBnn9TtEVngj?se=2025-01-28T17%3A54%3A45Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3Db939d99c-dfdd-4c0b-a641-b5d7ddc56b87.webp&sig=li5rwq%2BzHJIi0R7/Pu6md9FFmVpMTg%2B3R4h5F0nffS4%3D"
+                alt=""
+                width={50}
+              />
+              GenFit
+            </div>
+          </Link>
           <div className="flex gap-4 items-center">
-            <img
-              src="https://files.oaiusercontent.com/file-Hr8fa69sCxnBnn9TtEVngj?se=2025-01-28T17%3A54%3A45Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3Db939d99c-dfdd-4c0b-a641-b5d7ddc56b87.webp&sig=li5rwq%2BzHJIi0R7/Pu6md9FFmVpMTg%2B3R4h5F0nffS4%3D"
-              alt=""
-              width={50}
-            />
-            GenFit
+            <p>
+              <Link to="/home" className="hover:text-[#5D4037]">
+                Home
+              </Link>
+            </p>
+            <p>
+              <Link to="/series" className="hover:text-[#5D4037]">
+                Série
+              </Link>
+            </p>
+            <p>
+              <Link to="/sobre" className="hover:text-[#5D4037]">
+                Sobre nós
+              </Link>
+            </p>
           </div>
-          <div className="flex gap-4 items-center">Home Série Cadastro Sobre nós Sair</div>
         </div>
       </div>
     </>
--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -3,12 +3,12 @@ function Sobre() {
     <>
       <div className="bg-gradient-to-b from-black to-white flex justify-center min-h-screen">
         <div className="container flex text-white">
-          <div className="flex flex-col gap-4 items-start py-4">
+          <div className="flex flex-col gap-4 items-start py-20">
             <div className="flex  gap-4 items-center justify-center">
               <div className="flex flex-col gap-4 items-center justify-center">
-                <h2 className="text-5xl font-bold">Sobre nós</h2>
+                <h2 className="text-5xl font-bold" style={{ textShadow: '1px 1px 1px black' }}>Sobre nós</h2>
                 <br />
-                <p className="text-xl">
+                <p className="text-xl" style={{ textShadow: '1px 1px 1px black' }}>
                   Combinamos inovação e precisão técnica para desenvolver o
                   GenFit, uma plataforma de fitness personalizada que transforma
                   a maneira como as pessoas se conectam com sua saúde e
